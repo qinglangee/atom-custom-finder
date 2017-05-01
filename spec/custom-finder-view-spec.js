db79ad1237326e836499b48fcf0ca837ddf2104b
zhch <zhch@163.com>
2017-05-02 00:03:08 +0800

    init, copy from command palette

---
+++ spec/custom-finder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CustomFinderView from '../lib/custom-finder-view';
+
+describe('CustomFinderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
